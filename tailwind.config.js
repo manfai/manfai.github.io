693367193f3c9aa25878b4080c7d50d649671519
manfai <manfai106@gmail.com>
2021-09-14 17:01:08 +0800

    new version

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [
+    './_includes/**/*.html',
+    './_layouts/**/*.html',
+    './_posts/*.md',
+    './*.html',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    // require("@tailwindcss/forms"),
+    // require("@tailwindcss/typography"),
+    // require('daisyui'),
+  ],
+}
